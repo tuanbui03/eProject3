c2ed1f4a3400cb08824c41884f11f39908d7a0a4
Minnn9x <ngochien03111996@gmail.com>
2023-04-28 09:08:20 +0700

    update Controllers GalleriesController.cs, MoviesController.cs, SeatsController.cs

---
+++ ABCD_Admin/Controllers/GalleriesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ABCD_Admin.Models;
+
+namespace ABCD_Admin.Controllers
+{
+    public class GalleriesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Galleries
+        public ActionResult Index()
+        {
+            var gallery = db.Gallery.Include(g => g.Movies).Include(g => g.Products).Include(g => g.Shops);
+            return View(gallery.ToList());
+        }
+
+        // GET: Galleries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Gallery.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // GET: Galleries/Create
+        public ActionResult Create()
+        {
+            ViewBag.objectId = new SelectList(db.Movies, "movieId", "movieTitle");
+            ViewBag.objectId = new SelectList(db.Products, "productId", "productName");
+            ViewBag.objectId = new SelectList(db.Shops, "shopId", "shopName");
+            return View();
+        }
+
+        // POST: Galleries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "imageId,imagePath,objectType,objectId")] Gallery gallery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gallery.Add(gallery);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.objectId = new SelectList(db.Movies, "movieId", "movieTitle", gallery.objectId);
+            ViewBag.objectId = new SelectList(db.Products, "productId", "productName", gallery.objectId);
+            ViewBag.objectId = new SelectList(db.Shops, "shopId", "shopName", gallery.objectId);
+            return View(gallery);
+        }
+
+        // GET: Galleries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Gallery.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.objectId = new SelectList(db.Movies, "movieId", "movieTitle", gallery.objectId);
+            ViewBag.objectId = new SelectList(db.Products, "productId", "productName", gallery.objectId);
+            ViewBag.objectId = new SelectList(db.Shops, "shopId", "shopName", gallery.objectId);
+            return View(gallery);
+        }
+
+        // POST: Galleries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "imageId,imagePath,objectType,objectId")] Gallery gallery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gallery).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.objectId = new SelectList(db.Movies, "movieId", "movieTitle", gallery.objectId);
+            ViewBag.objectId = new SelectList(db.Products, "productId", "productName", gallery.objectId);
+            ViewBag.objectId = new SelectList(db.Shops, "shopId", "shopName", gallery.objectId);
+            return View(gallery);
+        }
+
+        // GET: Galleries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Gallery.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // POST: Galleries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gallery gallery = db.Gallery.Find(id);
+            db.Gallery.Remove(gallery);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ABCD_Admin/Controllers/SeatsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ABCD_Admin.Models;
+
+namespace ABCD_Admin.Controllers
+{
+    public class SeatsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Seats
+        public ActionResult Index()
+        {
+            return View(db.Seats.ToList());
+        }
+
+        // GET: Seats/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seats seats = db.Seats.Find(id);
+            if (seats == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seats);
+        }
+
+        // GET: Seats/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Seats/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "seatId,seatName,isVipSeat")] Seats seats)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seats.Add(seats);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seats);
+        }
+
+        // GET: Seats/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seats seats = db.Seats.Find(id);
+            if (seats == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seats);
+        }
+
+        // POST: Seats/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "seatId,seatName,isVipSeat")] Seats seats)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seats).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seats);
+        }
+
+        // GET: Seats/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seats seats = db.Seats.Find(id);
+            if (seats == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seats);
+        }
+
+        // POST: Seats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seats seats = db.Seats.Find(id);
+            db.Seats.Remove(seats);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
